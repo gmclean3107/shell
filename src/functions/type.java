91219e3bc34b325cfb0510dee18b56e76de06a1c
gmclean3107 <gman3107@outlook.com>
2026-07-25 19:25:05 +0100

    Implemented type for executable files/commands that can't be found.

--- src/functions/type.java
+++ src/functions/type.java
@@ -7,9 +7,14 @@ import java.util.List;
 public class type {
     public static void typeCommand (String input) {
         List<String> builtInFunctions = getBuiltIns();
+        String execPath = getExecPath(input);
 
         if (builtInFunctions.contains(input)) {
             System.out.println(input + " is a shell builtin");
+        } else if (execPath != null) {
+            System.out.println(input + " is " + execPath);
+        } else {
+            System.out.println(input + ": not found");
         }
 
     }
@@ -30,5 +35,31 @@ public class type {
         }
 
         return builtInFunctions;
+    }
+
+    private static String getExecPath(String input){
+        String path = System.getenv("PATH");
+        String[] extensions = System.getenv("PATHEXT").split(";");
+
+        if (path != null) {
+            String[] directories = path.split(File.pathSeparator);
+
+            for (String directory : directories) {
+                File exec = new File(directory, input);
+
+                if (exec.exists() && exec.canExecute()) {
+                    return exec.getPath();
+                }
+
+                for (String ext : extensions) {
+                    exec = new File(directory, input + ext);
+
+                    if (exec.exists() && exec.canExecute()) {
+                        return exec.getPath();
+                    }
+                }
+            }
+        }
+        return null;
     }
 }
